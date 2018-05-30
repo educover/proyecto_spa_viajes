2bb396917bf57f2d98aa657ab402ab65b5b9de85
Edu Alcover <alcover.edu@gmail.com>
2018-05-30 12:57:44 +0200

    estructura acabada

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('./bower_components',express.static(path.join(`${__dirname}public/components`)));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
